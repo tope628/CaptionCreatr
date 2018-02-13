1443f81c6654b38f9d728824535cf8990a55f1f0
Tope <tope628@gmail.com>
2018-02-13 08:07:26 +0000

    form input for flask request handled as well as random generator. lyrics showng in html jinja block

--- genius.py
+++ genius.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pprint import pprint
 import billboard
 from bs4 import BeautifulSoup
@@ -10,6 +10,7 @@ from sys import argv
 
 app = Flask(__name__)
 headers = {'Authorization': 'Bearer eCrneseMwLAhfmD8a2wUuKFHGV7N0ZSRSPUvSsenf5KK1JiYanWiF5xJxy1rTB0p'}
+search_url = "http://api.genius.com/search"
 
 
 @app.route('/')
@@ -17,7 +18,31 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/search')
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        hashtag = request.form['hashtag']
+        data = {'q': hashtag}
+    r = requests.get(search_url, params=data, headers=headers).json()
+    for hit in r["response"]["hits"]:
+            lyrics_path = hit["result"]["api_path"]
+            break
+    return render_template('lyrics.html', lyrics=print_lyrics(lyrics_path))
+
+
+@app.route('/random100', methods=['GET', 'POST'])
+def random100():
+    chart = billboard.ChartData('hot-100')
+    song = random.choice(chart)
+    data = {'q': song.title}
+
+    r = requests.get(search_url, params=data, headers=headers).json()
+    for hit in r["response"]["hits"]:
+            lyrics_path = hit["result"]["api_path"]
+            break
+    return render_template('lyrics.html', lyrics=print_lyrics(lyrics_path))
+
+
 def print_lyrics(lyrics_path):
     song_url = "http://api.genius.com" + lyrics_path
     r = requests.get(song_url, headers=headers).json()
@@ -25,24 +50,11 @@ def print_lyrics(lyrics_path):
     page_url = "http://genius.com" + path
     page = requests.get(page_url)
     html = BeautifulSoup(page.text, "html.parser")
-    lyrics = html.find("div", class_="lyrics").get_text().encode('ascii', 'ignore')
-    lyrics = str(lyrics).replace('\n', ' ')
+    lyrics = str(html.find("div", class_="lyrics").text)
+    lyrics = lyrics.replace('\n', '')
     return lyrics[:150]
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    search_url = "http://api.genius.com/search"
-    if len(argv) > 1:
-        hashtag = argv[1:]
-        data = {'q': hashtag}
-    else:
-        chart = billboard.ChartData('hot-100')
-        song = random.choice(chart)
-        data = {'q': song.title}
 
-    r = requests.get(search_url, params=data, headers=headers).json()
-    for hit in r["response"]["hits"]:
-            lyrics_path = hit["result"]["api_path"]
-            break
-    pprint(print_lyrics(lyrics_path))
